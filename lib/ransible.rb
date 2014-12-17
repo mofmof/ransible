8ef1101f2668ef23144a45856a62f99a0081802a
a.harada <redhornet96@gmail.com>
2014-12-17 17:31:18 +0900

    execute ansible

--- lib/ransible.rb
+++ lib/ransible.rb
@@ -1,5 +1,16 @@
 require "ransible/version"
 
 module Ransible
-  # Your code goes here...
+  class Executer
+  	def initialize(yml_path, inventory_path)
+      @yml_path = yml_path
+      @inventory_path = inventory_path
+  	end
+
+  	def run
+      log = `ansible-playbook -i #{@inventory_path} #{@yml_path}`
+      p "executed ansible"
+      { log: log }
+  	end
+  end
 end
